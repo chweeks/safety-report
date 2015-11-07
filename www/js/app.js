91fef6f6b0232ec713082b47fe7c55e2e9f9c03a
Aaron <aarongkendall@gmail.com>
2015-11-07 15:25:58 +0000

    all directives added

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+safetyapp = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,174 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+safetyapp.directive('aircraftConfiguration', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/aircraftConfiguration.html";
+
+    return directive;
+});
+
+safetyapp.directive('airproxTCAS', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/airproxTCAS.html";
+
+    return directive;
+});
+
+safetyapp.directive('altitudeDeviation', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/altitudeDeviation.html";
+
+    return directive;
+});
+
+safetyapp.directive('crewinformation', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/crewinformation.html";
+
+    return directive;
+});
+
+safetyapp.directive('egpws', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/EGPWS.html";
+
+    return directive;
+});
+
+safetyapp.directive('eventcategory', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/eventcategory.html";
+
+    return directive;
+});
+
+safetyapp.directive('eventdescription', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/eventdescription.html";
+
+    return directive;
+});
+
+safetyapp.directive('eventid', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/eventid.html";
+
+    return directive;
+});
+
+safetyapp.directive('fireSparksSmokeFumes', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/fireSparksSmokeFumes.html";
+
+    return directive;
+});
+
+safetyapp.directive('flightdetails', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/flightdetails.html";
+
+    return directive;
+});
+
+safetyapp.directive('flightidentification', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/flightidentification.html";
+
+    return directive;
+});
+
+safetyapp.directive('laserBeamQuestionnaire', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/laserBeamQuestionnaire.html";
+
+    return directive;
+});
+
+safetyapp.directive('maintenanceError', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/maintenanceError.html";
+
+    return directive;
+});
+
+safetyapp.directive('meterologicalinfo', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/meterologicalinfo.html";
+
+    return directive;
+});
+
+safetyapp.directive('reportinfo', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/reportinfo.html";
+
+    return directive;
+});
+
+safetyapp.directive('signup', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/signup.html";
+
+    return directive;
+});
+
+safetyapp.directive('systemFailure', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/systemFailure.html";
+
+    return directive;
+});
+
+safetyapp.directive('wakeTurbulence', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/wakeTurbulence.html";
+
+    return directive;
+});
+
+safetyapp.directive('wildlifeStrike', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../templates/wildlifeStrike.html";
+
+    return directive;
+});
