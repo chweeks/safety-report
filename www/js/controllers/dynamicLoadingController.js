d97986d21d989645c7d25b9a25b73eb52d911d8e
chweeks <chrisharryweeks@gmail.com>
2015-11-09 01:04:55 +0000

    added dynamic form views

--- www/js/controllers/dynamicLoadingController.js
+++ www/js/controllers/dynamicLoadingController.js
@@ -17,7 +17,7 @@ safetyapp.controller('DynamicLoadingCtrl', [ function() {
                               'Unstable Approach',
                               'Wake Turbulence' ]
 
-  aircraftconfiguration = function(eventCategory) {
+  self.aircraftconfiguration = function(eventCategory) {
     for(var i=0; i < aircraftConfigArray.length; i++){
       if(aircraftConfigArray[i] == eventCategory){
         return true;
@@ -26,55 +26,55 @@ safetyapp.controller('DynamicLoadingCtrl', [ function() {
     };
   };
 
-  airproxtcas = function(eventCategory) {
+  self.airproxtcas = function(eventCategory) {
     if (eventCategory == 'Airprox / TCAS') {
       return true
     }
   };
 
-  altitudedeviation = function(eventCategory) {
+  self.altitudedeviation = function(eventCategory) {
     if (eventCategory == 'Altitude Deviation') {
       return true
     }
   };
 
-  egpws = function(eventCategory) {
+  self.egpws = function(eventCategory) {
     if (eventCategory == 'EGPWS') {
       return true
     }
   };
 
-  laserbeamquestionnaire = function(eventCategory) {
-    if (eventCategory == 'Laser Beam exposure') {
+  self.laserbeamquestionnaire = function(eventCategory) {
+    if (eventCategory === 'Laser Beam Exposure') {
       return true
     }
   };
 
-  wildlifestrike = function(eventCategory) {
+  self.wildlifestrike = function(eventCategory) {
     if (eventCategory == 'Wildlife Strike') {
       return true
     }
   };
 
-  firesparkssmokefumes = function(eventCategory) {
+  self.firesparkssmokefumes = function(eventCategory) {
     if (eventCategory == 'Fire/Sparks/Smoke/Fumes') {
       return true
     }
   };
 
-  waketurbulence = function(eventCategory) {
+  self.waketurbulence = function(eventCategory) {
     if (eventCategory == 'Wake Turbulence') {
       return true
     }
   };
 
-  systemfailure = function(eventCategory) {
+  self.systemfailure = function(eventCategory) {
     if (eventCategory == 'System Failure') {
       return true
     }
   };
 
-  maintenanceerror = function(eventCategory) {
+  self.maintenanceerror = function(eventCategory) {
     if (eventCategory == 'maintenanceerror') {
       return true
     }
