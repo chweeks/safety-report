c13d7d750d20e7bc60a6c6be9cc77700c8333e33
chweeks <chrisharryweeks@gmail.com>
2015-11-18 14:09:54 +0000

    added tailNumber arrays

---
+++ www/js/controllers/aircraftTypeController.js
@@ -0,0 +1,39 @@
+safetyapp.controller('DynamicLoadingCtrl', [ function() {
+
+  var self = this;
+
+  var cessna = [CS-DXE, CS-DXF, CS-DXG, CS-DXH, CS-DXI,
+                CS-DXJ, CS-DXK, CS-DXL, CS-DXM, CS-DXN,
+                CS-DXO, CS-DXP, CS-DXQ, CS-DXR, CS-DXS,
+                CS-DXT, CS-DXU, CS-DXV, CS-DXW, CS-DXX,
+                CS-DXY, CS-DXZ, CS-DQA, CS-DQB];
+
+  var hawkBeechXpc = [CS-DRG, CS-DRH, CS-DRI, CS-DRL, CS-DRN,
+                      CS-DRO, CS-DRP, CS-DRQ, CS-DRR, CS-DRS,
+                      CS-DRT, CS-DRU, CS-DRV, CS-DRW, CS-DRX,
+                      CS-DRY, CS-DRZ];
+
+  var hawkBeechSevenFifty = [CS-DUA, CS-DUB, CS-DUC, CS-DUD, CS-DUE,
+                             CS-DUF, CS-DUG, CS-DUH];
+
+  var falcon = [CS-DFD, CS-DNR, CS-DNS];
+
+  var falconEasy = [CS-DFF, CS-DFG, CS-DFK, CS-DLB, CS-DLC,
+                    CS-DLD, CS-DLE, CS-DLF, CS-DLG, CS-DLH];
+
+  var falconX = [CS-DSB, CS-DSD];
+
+  var gulfstream = [CS-DKD, CS-DKE, CS-DKF, CS-DKG, CS-DKH
+                    CS-DKI, CS-DKJ, CS-DKK];
+
+  var bombardierFiveThous = [CS-TSL];
+
+  var bombardierSixThous = [CS-GLA, CS-GLB, CS-GLC, CS-GLD, CS-GLE,
+                            CS-GLF];
+
+  var bombardierThreeFifty = [CS-GLA, CS-GLB, CS-GLC];
+
+  var embraer = [CS-PHA, CS-PHB, CS-PHC, CS-PHD, CS-PHE,
+                 CS-PHF, CS-PHG, CS-PHH];
+
+}])
