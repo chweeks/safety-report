60f7e1de15fbf6b1e17ad6bead4e73ee481e2a8f
chweeks <chrisharryweeks@gmail.com>
2015-12-02 14:43:03 +0000

    updated sign up controller

--- www/js/controllers/signUpController.js
+++ www/js/controllers/signUpController.js
@@ -1,8 +1,8 @@
 safetyapp.controller('signUpCtrl', ['$http', '$cookies', '$state', function($http, $cookies, $state) {
   var self = this;
 
-  self.postSignUp = function(email, password, passwordconf, name, address, primaryphone, secondaryphone, rank, crewmemtype, otheremployee) {
-    var postData = { 'email': email, 'password': password, 'password_confirmation': passwordconf, 'name': name, 'address': address, 'primary_phone': primaryphone, 'secondary_phone': secondaryphone, 'rank': rank, 'crew_member_aircraft_type': crewmemtype, 'other_employee_name': otheremployee};
+  self.postSignUp = function(username, password, passwordconf) {
+    var postData = { 'username': username, 'password': password, 'password_confirmation': passwordconf};
       $http.post('https://immense-castle-3453.herokuapp.com/users', postData, 'POST').success(function(data) {
         $cookies.put("auth_key", data.auth_key);
         $cookies.put('user_id', data.user_id);
